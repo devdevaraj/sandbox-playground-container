4650393933c646454b8f3ad76d04021767075b9c
devdevaraj <devarajperayil327@gmail.com>
2025-10-23 11:25:20 +0000

    Fix code server problem

--- firestarter/http_proxy/http_proxy.go
+++ firestarter/http_proxy/http_proxy.go
@@ -1,23 +1,21 @@
 package httpproxy
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
 
 	wsserver "github.com/devdevaraj/firestarter/ws_server"
-	"github.com/gorilla/mux"
 )
 
 func HTTPProxy(port string) {
-	router := mux.NewRouter()
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Log for each request")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		subdomain := strings.Split(r.Host, ".")[0]
 		split := strings.Split(subdomain, "-")
 		id := split[len(split)-1]
+
+		log.Println(r.Header)
 
 		reverseProxy, ok := wsserver.GetProxy(id)
 		if ok {
@@ -29,7 +27,7 @@ func HTTPProxy(port string) {
 	})
 
 	log.Println("Reverse proxy server listening on port " + port)
-	if err := http.ListenAndServe(":"+port, router); err != nil {
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal("ListenAndServe ("+port+"): ", err)
 	}
 }
