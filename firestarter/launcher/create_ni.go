1180b3214f1cbda9fe9b1feea83509934b78ff34
Devaraj <devarajperayil327@gmail.com>
2025-11-01 14:15:59 +0530

    Change network configuration

--- firestarter/launcher/create_ni.go
+++ firestarter/launcher/create_ni.go
@@ -1,6 +1,7 @@
 package launcher
 
 import (
+	"log"
 	"net"
 
 	"github.com/devdevaraj/firestarter/init_app"
@@ -25,6 +26,7 @@ import (
 func CreateNetworkInterface(networks []init_app.Network) []firecracker.NetworkInterface {
 	nics := make([]firecracker.NetworkInterface, 0, len(networks))
 	for i, network := range networks {
+		log.Println(*network.IP + network.TAP + network.Name + *network.Bridge)
 		var nic firecracker.NetworkInterface
 		if i == 0 && network.IP != nil && *network.IP != "" {
 			nic = firecracker.NetworkInterface{
