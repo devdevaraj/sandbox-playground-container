c79cec9abb710676c63902a6d4bfe3e5d01b6fd3
Devaraj <devarajperayil327@gmail.com>
2025-11-03 15:04:40 +0530

    Change network configuration

--- firestarter/ws_server/ip_list.go
+++ firestarter/ws_server/ip_list.go
@@ -2,6 +2,7 @@ package wsserver
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 
 	"github.com/devdevaraj/firestarter/init_app"
@@ -41,6 +42,7 @@ func PopulateIp(cfg init_app.Config) {
 				name: fmt.Sprintf("vm%d", 1+i),
 			})
 			if *vm.EnableIDE {
+				log.Printf(strconv.Itoa(*vm.IDEPort))
 				TARGET_CACHE["codeserverprt"] = *ni.IP + ":" + strconv.Itoa(*vm.IDEPort)
 			}
 		}
