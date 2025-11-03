fb73fece9a951b24859120c622a1196582abc93c
Devaraj <devarajperayil327@gmail.com>
2025-11-03 14:50:59 +0530

    Change network configuration

--- firestarter/ws_server/handle_check.go
+++ firestarter/ws_server/handle_check.go
@@ -13,7 +13,7 @@ func HandleCheck(w http.ResponseWriter, r *http.Request, no_vms int) {
 	errChan := make(chan error, 2)
 
 	timeout := 10 * time.Second
-	retryInterval := 250 * time.Millisecond
+	retryInterval := 200 * time.Millisecond
 
 	// vms := makeVMs(no_vms)
 
